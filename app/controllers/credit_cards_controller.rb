10a6034db2d76417377783d81aa30d913d026912
mikebutts <mgbutts@gmail.com>
2015-07-28 20:09:41 -0400

    finishing users

---
+++ app/controllers/credit_cards_controller.rb
@@ -0,0 +1,20 @@
+class CreditCardsController < ApplicationController
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.credit_card.destroy
+    if @user.credit_card.destroy
+      flash[:success] = "Credit Card successfully deleted!"
+      redirect_to products_path
+    else
+      flash.now[:danger] = 'Oops, there was an error deleting your credit card.'
+      
+    end
+    redirect_to users_path
+  end
+
+  def show
+    @credit_card = User.find(params[:id]).credit_card
+  end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
+  
   def addresses
   end
 
   def unplaced
-
+    @user = User.find(params[:id])
   end
+
+
 end
